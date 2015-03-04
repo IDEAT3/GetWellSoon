6917b101b0514e6b15c5ba70a053b7218ded3761
abhijithka <kaabhijith27.7@gmail.com>
2015-03-04 14:10:53 +0530

    Configuration file for database

---
+++ lib/configure.php
@@ -0,0 +1,14 @@
+<?php
+	$servername = "localhost";
+	$username = "admin";
+	$password = "password";
+	$dbname = "health_centre";
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	else 
+	{ 
+		echo("connection successful");
+	}
+?>
