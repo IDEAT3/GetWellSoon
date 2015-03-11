52777099fffa27a2ead9d9a5d59d0f2f5f1f3688
abhijithka <kaabhijith27.7@gmail.com>
2015-03-11 21:03:25 +0530

    Configuration file included instead of directly giving username and password, connection renamed to conn

--- lib/login.php
+++ lib/login.php
@@ -10,22 +10,22 @@ if (isset($_POST['submit'])) {
 		$password=$_POST['password'];
 //	echo $username;
 //	echo $password;
-		$connection = mysql_connect("localhost", "admin", "password") or die("Failed to connect to MySQL: " . mysql_error());
+		include("configure.php");
 		$username = stripslashes($username);
 		$password = stripslashes($password);
 		$username = mysql_real_escape_string($username);
 		$password = mysql_real_escape_string($password);
 ////		$password = AES_ENCRYPT($password,$password);  need a new function
-		$db = mysql_select_db("health_centre", $connection) or die("Failed to connect to db: " . mysql_error());
+		$db = mysql_select_db("health_centre", $conn) or die("Failed to connect to db: " . mysql_error());
 		if(!$db) {echo "lost";};
-		$query = mysql_query("SELECT * FROM member WHERE password='$password' AND username='$username'", $connection) or die("Failed to connect to table: " . mysql_error());
+		$query = mysql_query("SELECT * FROM member WHERE password='$password' AND username='$username'", $conn) or die("Failed to connect to table: " . mysql_error());
 		$rows = mysql_num_rows($query);
 		if ($rows == 1) {
 			$_SESSION['login_user']=$username; 
 			header("location: home.php"); 
 		} else {
 			$error = "*Username or Password is invalid";
-			mysql_close($connection);
+			mysql_close($conn);
 		} 
 	
 	}
