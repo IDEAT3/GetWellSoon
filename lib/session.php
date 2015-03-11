61a288bddfb7bfeae2caa06476bcd05dc1c2920d
abhijithka <kaabhijith27.7@gmail.com>
2015-03-11 21:12:36 +0530

    configure.php included and  used instead of

--- lib/session.php
+++ lib/session.php
@@ -1,14 +1,14 @@
 <?php
-	$connection = mysql_connect("localhost", "admin", "password");
-	$db = mysql_select_db("health_centre", $connection);
+	include(configure.php);
+	$db = mysql_select_db("health_centre", $conn);
 	session_start();
 	$user_check=$_SESSION['login_user'];
-	$ses_sql=mysql_query("SELECT * FROM member WHERE username='$user_check'", $connection);
+	$ses_sql=mysql_query("SELECT * FROM member WHERE username='$user_check'", $conn);
 	$row = mysql_fetch_assoc($ses_sql);
 	$login_session =$row['username'];
 	$login_type=$row['type'];
 	if(!isset($login_session)) {
-		mysql_close($connection); 
+		mysql_close($conn); 
 		header('Location: ../index.php'); 
 	}
 ?>
